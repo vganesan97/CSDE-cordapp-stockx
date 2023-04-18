3c1dd9e1cfc8180c2756a98e019f5cc7a0e2f467
Austin Moothart <austin.moothart@r3.com>
2023-04-18 16:45:37 -0400

    refactor out finalize digital currency generic subflow

--- workflows/src/main/kotlin/com/r3/developers/csdetemplate/digitalcurrency/workflows/IssueDigitalCurrencyFlow.kt
+++ workflows/src/main/kotlin/com/r3/developers/csdetemplate/digitalcurrency/workflows/IssueDigitalCurrencyFlow.kt
@@ -2,12 +2,11 @@ package com.r3.developers.csdetemplate.digitalcurrency.workflows
 
 import com.r3.developers.csdetemplate.digitalcurrency.contracts.DigitalCurrencyContract
 import com.r3.developers.csdetemplate.digitalcurrency.states.DigitalCurrency
-import net.corda.v5.application.flows.ClientRequestBody
-import net.corda.v5.application.flows.ClientStartableFlow
-import net.corda.v5.application.flows.CordaInject
-import net.corda.v5.application.flows.FlowEngine
+import net.corda.v5.application.flows.*
 import net.corda.v5.application.marshalling.JsonMarshallingService
 import net.corda.v5.application.membership.MemberLookup
+import net.corda.v5.application.messaging.FlowMessaging
+import net.corda.v5.application.messaging.FlowSession
 import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.base.exceptions.CordaRuntimeException
 import net.corda.v5.base.types.MemberX500Name
@@ -20,6 +19,7 @@ import java.time.Instant
 
 data class IssueDigitalCurrency(val quantity: Int, val holder: String)
 
+@InitiatingFlow(protocol = "finalize-issue-digital-currency-protocol")
 class IssueDigitalCurrencyFlow: ClientStartableFlow {
 
     private companion object {
@@ -39,7 +39,7 @@ class IssueDigitalCurrencyFlow: ClientStartableFlow {
     lateinit var ledgerService: UtxoLedgerService
 
     @CordaInject
-    lateinit var flowEngine: FlowEngine
+    lateinit var flowMessaging: FlowMessaging
 
     @Suspendable
     override fun call(requestBody: ClientRequestBody): String {
@@ -68,11 +68,47 @@ class IssueDigitalCurrencyFlow: ClientStartableFlow {
 
             val signedTransaction = txBuilder.toSignedTransaction()
 
-            return flowEngine.subFlow(FinalizeDigitalCurrencySubFlow(signedTransaction, holder.name))
+            val session = flowMessaging.initiateFlow(holder.name)
 
+            val finalizedSignedTransaction = ledgerService.finalize(
+                signedTransaction,
+                listOf(session)
+            )
+            return finalizedSignedTransaction.id.toString().also {
+                log.info("Successful ${signedTransaction.commands.first()} with response: $it")
+            }
         }
         catch (e: Exception) {
             log.warn("Failed to process issue digital currency for request body '$requestBody' with exception: '${e.message}'")
+            throw e
+        }
+    }
+}
+
+@InitiatedBy(protocol = "finalize-issue-digital-currency-protocol")
+class FinalizeIssueDigitalCurrencyResponderFlow: ResponderFlow {
+    private companion object {
+        val log = LoggerFactory.getLogger(this::class.java.enclosingClass)
+    }
+
+    @CordaInject
+    lateinit var ledgerService: UtxoLedgerService
+
+    @Suspendable
+    override fun call(session: FlowSession) {
+        log.info("${this::class.java.enclosingClass}.call() called")
+
+        try {
+            val finalizedSignedTransaction = ledgerService.receiveFinality(session) { ledgerTransaction ->
+                val state = ledgerTransaction.getOutputStates(DigitalCurrency::class.java).singleOrNull() ?:
+                throw CordaRuntimeException("Failed verification - transaction did not have exactly one output DigitalCurrency.")
+
+                log.info("Verified the transaction- ${ledgerTransaction.id}")
+            }
+            log.info("Finished issue digital currency responder flow - ${finalizedSignedTransaction.id}")
+        }
+        catch (e: Exception) {
+            log.warn("Issue DigitalCurrency responder flow failed with exception", e)
             throw e
         }
     }
--- workflows/src/main/kotlin/com/r3/developers/csdetemplate/digitalcurrency/workflows/TransferDigitalCurrencyFlow.kt
+++ workflows/src/main/kotlin/com/r3/developers/csdetemplate/digitalcurrency/workflows/TransferDigitalCurrencyFlow.kt
@@ -2,12 +2,11 @@ package com.r3.developers.csdetemplate.digitalcurrency.workflows
 
 import com.r3.developers.csdetemplate.digitalcurrency.contracts.DigitalCurrencyContract
 import com.r3.developers.csdetemplate.digitalcurrency.states.DigitalCurrency
-import net.corda.v5.application.flows.ClientRequestBody
-import net.corda.v5.application.flows.ClientStartableFlow
-import net.corda.v5.application.flows.CordaInject
-import net.corda.v5.application.flows.FlowEngine
+import net.corda.v5.application.flows.*
 import net.corda.v5.application.marshalling.JsonMarshallingService
 import net.corda.v5.application.membership.MemberLookup
+import net.corda.v5.application.messaging.FlowMessaging
+import net.corda.v5.application.messaging.FlowSession
 import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.base.exceptions.CordaRuntimeException
 import net.corda.v5.base.types.MemberX500Name
@@ -21,6 +20,7 @@ import java.time.Instant
 
 data class TransferDigitalCurrency(val quantity: Int, val toHolder: String)
 
+@InitiatingFlow(protocol = "finalize-transfer-digital-currency-protocol")
 class TransferDigitalCurrencyFlow: ClientStartableFlow {
     private companion object {
         val log = LoggerFactory.getLogger(this::class.java.enclosingClass)
@@ -39,7 +39,7 @@ class TransferDigitalCurrencyFlow: ClientStartableFlow {
     lateinit var ledgerService: UtxoLedgerService
 
     @CordaInject
-    lateinit var flowEngine: FlowEngine
+    lateinit var flowMessaging: FlowMessaging
 
     @Suspendable
     override fun call(requestBody: ClientRequestBody): String {
@@ -100,12 +100,59 @@ class TransferDigitalCurrencyFlow: ClientStartableFlow {
 
             val signedTransaction = txBuilder.toSignedTransaction()
 
-            return flowEngine.subFlow(FinalizeDigitalCurrencySubFlow(signedTransaction, toHolder.name))
+            val session = flowMessaging.initiateFlow(toHolder.name)
+
+            val finalizedSignedTransaction = ledgerService.finalize(
+                signedTransaction,
+                listOf(session)
+            )
+            return finalizedSignedTransaction.id.toString().also {
+                log.info("Successful ${signedTransaction.commands.first()} with response: $it")
+            }
         }
         catch (e: Exception) {
             log.warn("Failed to process transfer digital currency for request body '$requestBody' with exception: '${e.message}'")
             throw e
         }
     }
-
 }
+
+@InitiatedBy(protocol = "finalize-transfer-digital-currency-protocol")
+class FinalizeTransferDigitalCurrencyResponderFlow: ResponderFlow {
+    private companion object {
+        val log = LoggerFactory.getLogger(this::class.java.enclosingClass)
+    }
+
+    @CordaInject
+    lateinit var ledgerService: UtxoLedgerService
+
+    @Suspendable
+    override fun call(session: FlowSession) {
+        log.info("${this::class.java.enclosingClass}.call() called")
+
+        try {
+            val finalizedSignedTransaction = ledgerService.receiveFinality(session) { ledgerTransaction ->
+                val state = ledgerTransaction.getOutputStates(DigitalCurrency::class.java).first() ?:
+                    throw CordaRuntimeException("Failed verification - transaction did not have at least one output DigitalCurrency.")
+
+                log.info("Verified the transaction- ${ledgerTransaction.id}")
+            }
+            log.info("Finished transfer digital currency responder flow - ${finalizedSignedTransaction.id}")
+        }
+        catch (e: Exception) {
+            log.warn("Transfer DigitalCurrency responder flow failed with exception", e)
+            throw e
+        }
+    }
+}
+
+/*
+{
+    "clientRequestId": "transfer-1",
+    "flowClassName": "com.r3.developers.csdetemplate.digitalcurrency.workflows.TransferDigitalCurrencyFlow",
+    "requestBody": {
+        "quantity":30,
+        "toHolder":"CN=Bank of Bob, OU=Test Dept, O=R3, L=NYC, C=US"
+    }
+}
+ */
